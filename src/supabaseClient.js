14378ec21ae334e4fc02371bf5d960e857e995ce
Arjun Biju <arjunbiju332@gmail.com>
2025-05-20 20:43:47 +0530

    Database changed

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://bscypztmkhtbxemdkuis.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzY3lwenRta2h0YnhlbWRrdWlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3NDYzNzksImV4cCI6MjA2MzMyMjM3OX0.3-_3c1mDYEet0R44-wT_1_3jB8G1Ul1UlG35vvaK-r0';
+const supabaseUrl = 'https://kzhbmjygrzjardgruunp.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6aGJtanlncnpqYXJkZ3J1dW5wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3NTI5MDMsImV4cCI6MjA2MzMyODkwM30.3Z-xjElKc24Iz6tqSkwsAKpcuFmeAihOqigi3cWtI5g';
 export const supabase = createClient(supabaseUrl, supabaseKey);
